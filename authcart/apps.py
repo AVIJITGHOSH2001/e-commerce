4ad50ebb3bb5eb497e9dbf7606e0823c7ce543f3
Avijit Ghosh <avijitghoshcollege@gmail.com>
2023-07-16 19:19:27 +0530

    first commit

---
+++ authcart/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthcartConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authcart'
---
+++ ecommerceapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from ecommerceapp import views
+
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('contact/',views.contact,name='contact'),
+    path('about/',views.about,name='about'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('handlerequest/',views.handlerequest,name="Handlerequest"),
+
+
+
+
+
+
+
+]
